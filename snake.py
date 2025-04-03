f5238aea77afdbb77d14763de8bfcdf441d577bc
mavin321 <mavinpeteromondi@gmail.com>
2025-04-03 20:12:18 +0300

    version 2

--- snake.py
+++ snake.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.locals import *
 import time
+import random
 
 pygame.init()#initializing the game
 pygame.mixer.init() #initialize background music
@@ -34,8 +35,16 @@ Black=(0, 0, 0)
 
 #reset game
 def reset_game():
+    apple_group.empty()
     my_snake.rect.x=screen_width//2
     my_snake.rect.y=screen_height//2
+    score=0
+    return score
+
+#show score
+def draw_text(text,font,text_col,x ,y):
+    img=font.render(text, True, text_col)
+    screen.blit(img,(x,y))
 
 
 #create a class for the snake object
@@ -94,12 +103,22 @@ class Button_for_reset():
 
         screen.blit(self.image, (self.rect.x, self.rect.y))
         return action
+    
+class Apple(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image=pygame.image.load("assets/apple.png")
+        self.rect=self.image.get_rect()
+        self.rect.left=x
+        self.rect.top=y
+    
 
 
 
             
 #creating groups
 snake_group=pygame.sprite.Group()
+apple_group=pygame.sprite.Group()
 #create an instance of the object
 my_snake=Snake(screen_width//2,screen_height//2)
 #adding to group
@@ -119,6 +138,7 @@ while snake:
     #placing the snake on the screen
     snake_group.draw(screen)
     snake_group.update()
+    apple_group.draw(screen)
 
     #CHECKING IF THE SNAKE IS WITHIN BOUNDS
     if my_snake.rect.top==60 or my_snake.rect.top==600 or my_snake.rect.left==60 or my_snake.rect.left==900:
@@ -129,6 +149,27 @@ while snake:
 
     pygame.draw.rect(screen, Black, (50,50,900,600), 10)
 
+    #display score
+    draw_text(str(score),font, white, int(screen_width/2),20)
+
+
+    #generating apples
+    if game_over==False and slither==True:
+        if score==0 and len(apple_group)==0:
+            apple_x=random.randint(65,890)
+            apple_y=random.randint(65,590)
+            apple_time=Apple(apple_x,apple_y)
+            apple_group.add(apple_time)
+        elif pygame.sprite.groupcollide(apple_group, snake_group,True,False):
+            score+=1
+            apple_x=random.randint(65,890)
+            apple_y=random.randint(65,590)
+            apple_time=Apple(apple_x,apple_y)
+            apple_group.add(apple_time)
+       
+
+
+
     #checking for game over for reset
     if game_over==True:
         if button.draw()==True:
@@ -137,7 +178,7 @@ while snake:
             my_snake.vel=2
             my_snake.image=pygame.image.load('assets/head_down.png')
             my_snake.direction='down'
-            reset_game()
+            score=reset_game()
 
 
     for event in pygame.event.get():
